5c97e3113e01fe54387445a9088027fa734b854f
chuck hutchinson <chuckhutchinson2@yahoo.com>
2017-04-16 18:46:39 -0400

    added jsonoutput

--- src/main/groovy/org/location/controller/LocationController.groovy
+++ src/main/groovy/org/location/controller/LocationController.groovy
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.http.MediaType
 
 import groovy.util.logging.Slf4j
+import groovy.json.JsonOutput
 
 @Slf4j
 @RestController
@@ -21,27 +21,27 @@ class LocationController {
 	@Autowired
 	LocationService locationService
 
-	@RequestMapping(value = "/locations/{zipCode}", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@RequestMapping(value = "/locations/{zipCode}", method=RequestMethod.GET, produces="application/json")
 	public @ResponseBody String locations(@PathVariable String zipCode) {
-		return locationService.findZipCode(zipCode)
+		return JsonOutput.toJson(locationService.findZipCode(zipCode))
 	}
 
-	@RequestMapping(value = "/cities/{state}", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@RequestMapping(value = "/cities/{state}", method=RequestMethod.GET, produces="application/json")
 	public @ResponseBody String cities(@PathVariable String state) {
-		return locationService.findCities(state)
+		return JsonOutput.toJson(locationService.findCities(state))
 	}
 
 	// /near/38.9896/77.4505/5
-	@RequestMapping(value = "/near/{lat}/{lon}/{distance}", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@RequestMapping(value = "/near/{lat}/{lon}/{distance}", method=RequestMethod.GET, produces="application/json")
 	public @ResponseBody String near(@PathVariable String lat, @PathVariable String lon, @PathVariable Double distance) {
 		log.info("lat " + lat + " lon " + lon + " distance " + distance)
-		return locationService.withinLatLon(lat, lon, distance)
+		return JsonOutput.toJson(locationService.withinLatLon(lat, lon, distance))
 	}
 	
-	@RequestMapping(value = "/near/{zipCode}/{distance}", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@RequestMapping(value = "/near/{zipCode}/{distance}", method=RequestMethod.GET, produces="application/json")
 	public @ResponseBody String near(@PathVariable String zipCode,  @PathVariable Double distance) {
 		def location = locationService.findZipCode(zipCode)
 		
-		return locationService.within(location[0], distance)
+		return JsonOutput.toJson(locationService.within(location[0], distance))
 	}
 }
